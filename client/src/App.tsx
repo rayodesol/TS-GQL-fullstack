acb7e85f8e35816f6c84fe57ba5c12ecbd9e384d
song <song@zensystems.co.kr>
2024-05-03 15:56:42 +0900

    [ADD] 프론트엔드 세팅 - React, Typescript, Ant Design

---
+++ client/src/App.tsx
@@ -0,0 +1,39 @@
+// https://ant.design/docs/react/use-with-create-react-app
+import React from 'react';
+import { Button } from 'antd';
+
+const App: React.FC = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default App;
+
+// 초기 React 코드
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
