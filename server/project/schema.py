8b2163b8aabd5e168f47ce0507b78d830c2f4b08
song <song@zensystems.co.kr>
2024-05-13 14:41:07 +0900

    [MODIFY] 회사 프로젝트 구조처럼 schema.py 파일 수정

---
+++ server/project/schema.py
@@ -0,0 +1,19 @@
+import strawberry
+from strawberry_django.optimizer import DjangoOptimizerExtension
+
+from server.apps.gridCRUD.graphql.schema import GridCRUDQuery
+
+
+# 클래스 상속으로 각 장고 앱의 스키마 내용을 한 곳에 모음
+# 클래스 상속; https://wikidocs.net/16073
+@strawberry.type
+class Query(GridCRUDQuery):  # class 자식클래스(부모클래스)
+    """Query Root"""
+
+
+schema = strawberry.Schema(
+    query=Query,
+    extensions=[
+        DjangoOptimizerExtension,
+    ],
+)
