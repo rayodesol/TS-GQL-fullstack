81436611bef3e7733196c7e65be78f5d04b31781
song <song@zensystems.co.kr>
2024-05-14 18:07:25 +0900

    [ADD] FE 파일 구조 대략적 구성

--- client/src/pages/GridCRUD/index.tsx
+++ client/src/pages/GridCRUD/index.tsx
@@ -1,7 +1,46 @@
-import React, { PureComponent } from "react";
+// https://ant.design/components/table#how-to-use
+import * as React from "react";
+import Navbar from "../../components/Navbar";
+import { Table } from "antd";
 
-export default class index extends PureComponent {
-  render() {
-    return <div>테스트용 임시 코드</div>;
-  }
+const dataSource = [
+  {
+    key: "1",
+    name: "Mike",
+    age: 32,
+    address: "10 Downing Street",
+  },
+  {
+    key: "2",
+    name: "John",
+    age: 42,
+    address: "10 Downing Street",
+  },
+];
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Table dataSource={dataSource} columns={columns} />
+    </div>
+  );
 }
